9225f17cf523f9b83dd78798e48d11eaac299956
Martina <martina.heili@alumnos.udit.es>
2025-10-24 15:49:25 +0200

    Empezamos con la home

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// CAMBIO DE COLOR BOTONES
+
+$(document).ready(function(){
+  $('.color-btn').click(function(){
+    // quitar active del resto
+    $('.color-btn').removeClass('active');
+    $(this).addClass('active');
+
+    // obtener src de data attributes
+    const img1 = $(this).data('img1');
+    const img2 = $(this).data('img2');
+
+    // cambiar imágenes
+    $('.color-img-1').attr('src', img1);
+    $('.color-img-2').attr('src', img2);
+  });
+});
