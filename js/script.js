ffa0f2a6b1bcb33e413ff8bddaddb7075f57144e
laaulozaano <laura.lozano@alumnos.udit.es>
2025-11-19 02:26:34 +0100

    responsives

--- js/script.js
+++ js/script.js
@@ -186,6 +186,15 @@ $(document).ready(function () {
 //   "<"
 // );
 
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll('.fade-in');
+
+  elements.forEach((el, i) => {
+    setTimeout(() => {
+      el.classList.add('show');
+    }, i * 150); // Delay escalonado elegante
+  });
+});
 
 
 // ANIMACION FOTO DE CHIP
