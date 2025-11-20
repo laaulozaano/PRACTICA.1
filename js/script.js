939150fca40495d1675edd9620c036ca59ca34dc
laaulozaano <laura.lozano@alumnos.udit.es>
2025-11-20 14:59:39 +0100

    últimos ajustillos

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
     const myModal = new bootstrap.Modal(document.getElementById('discountModal'));
     myModal.show();
-  }, 3000); // se abre a los 3 segundos
+  }, 3000); 
 });
 
 $(document).ready(function () {
@@ -51,23 +51,17 @@ $(document).ready(function () {
 
 
 // ----------ANIMACIÓN CAMBIO DE IMAGEN COLOR----------
-
 $(document).ready(function () {
   $('.color-btn').click(function () {
-    // Quitar clase activa del resto
     $('.color-btn').removeClass('active');
     $(this).addClass('active');
 
-    // Obtener las nuevas imágenes del botón pulsado
     const img1Src = $(this).data('img1');
     const img2Src = $(this).data('img2');
 
-    // Fade out ambas imágenes al mismo tiempo
     $('.color-img-1, .color-img-2').stop(true, true).fadeTo(250, 0, function () {
-      // Cambiar src
       $('.color-img-1').attr('src', img1Src);
       $('.color-img-2').attr('src', img2Src);
-      // Fade in
       $('.color-img-1, .color-img-2').fadeTo(250, 1);
     });
   });
@@ -78,11 +72,11 @@ $(document).ready(function () {
 
 document.addEventListener("DOMContentLoaded", function () {
   const swiper = new Swiper(".mySwiper", {
-    slidesPerView: "auto",  // ancho de cada slide según CSS
+    slidesPerView: "auto",  
     spaceBetween: 20,
-    freeMode: true,          // permite drag natural
+    freeMode: true, 
     grabCursor: true,
-    loop: false,             // sin loop
+    loop: false,             
     autoplay: false,
     centeredSlides: false,
     watchOverflow: true
@@ -131,10 +125,7 @@ $(document).ready(function () {
     });
   }
 
-  // Cada vez que cambia un checkbox
   $('.filter-checkbox').change(applyFilters);
-
-  // Botón Clear Filters
   $('#clearFilters').click(function () {
     $('.filter-checkbox').prop('checked', false);
     applyFilters();
@@ -142,56 +133,13 @@ $(document).ready(function () {
 });
 
 
-
-// product page----------
-// gsap.registerPlugin(ScrollTrigger);
-
-// let heroTL = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: ".hero-2",
-//     start: "top top",
-//     end: "bottom+=150% top",
-//     scrub: 1.5,
-//     pin: true,
-//     anticipatePin: 1,
-//     invalidateOnRefresh: true,
-//     onUpdate: (self) => {
-//       if (self.progress > 0.75) {
-//         document.querySelector(".price-product").classList.add("visible");
-//         document.querySelector(".product-options").classList.add("visible");
-//       } else {
-//         document.querySelector(".price-product").classList.remove("visible");
-//         document.querySelector(".product-options").classList.remove("visible");
-//       }
-//     }
-//   }
-// });
-
-
-// Animación del título
-// heroTL
-//   .fromTo(".product-name-h1",
-//     { scale: 1, yPercent: 0 },
-//     { scale: 1, yPercent: -20, ease: "power2.inOut" }
-//   )
-//   .to(".product-name-h1",
-//     { fontSize: '4vw', yPercent: -5, scale: 1, ease: "power2.inOut" }
-//   );
-
-// Animación de la imagen
-// heroTL.fromTo(".product-image",
-//   { scale: 1, xPercent: 0, yPercent: 0 },
-//   { scale: 1.1, xPercent: 60, yPercent: 5, ease: "power2.inOut" },
-//   "<"
-// );
-
 document.addEventListener("DOMContentLoaded", () => {
   const elements = document.querySelectorAll('.fade-in');
 
   elements.forEach((el, i) => {
     setTimeout(() => {
       el.classList.add('show');
-    }, i * 150); // Delay escalonado elegante
+    }, i * 150); 
   });
 });
 
@@ -211,8 +159,7 @@ gsap.utils.toArray(".chip-img").forEach((img, i) => {
       toggleActions: "play none none reverse"
     }
   });
-
-  // Pequeña flotación mientras aparecen
+  
   gsap.to(img, {
     y: -15,
     duration: 3,
